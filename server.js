54369dcf20d477730668b28d0f7551a5e3628a2b
Javier Ontiveros <javieront2000@gmail.com>
2025-01-02 11:41:53 -0600

    feat: new route to store error logs from esp32

--- server.js
+++ server.js
@@ -10,21 +10,9 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zYm9oaWtobmRhcHNkeGxrc3lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMzMjY4NzEsImV4cCI6MjA0ODkwMjg3MX0.O8n0Rej7DdRwvkK_3tHs5r9kkmm1g8oTiiAGZiVcsHg"
 );
 
-app.get("/", async (req, res) => {
-  try {
-    const { error } = await supabase.from("sensors-data").insert({
-      moisture: 0.0,
-      temperature: 0.0,
-    });
-    res.status(200).json({ status: "success" });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.post("/store-moisture-average", async (req, res) => {
+app.post("/store-sensor-readings", async (req, res) => {
   const { moisture_average, temperature_average, battery_level } = req.body;
-  const { error } = await supabase.from("sensors-data").insert({
+  const { error } = await supabase.from("sensor-readings-prototype-v1").insert({
     moisture: moisture_average,
     temperature: temperature_average,
     battery_level: battery_level,
@@ -32,4 +20,12 @@ app.post("/store-moisture-average", async (req, res) => {
   res.status(200).json({ status: "success" });
 });
 
-app.listen(3000, () => console.log("Example app is listening on port 3000."));
+app.post("/store-error-logs", async (req, res) => {
+  const { esp32_error } = req.body;
+  const { error } = await supabase.from("errors-logs").insert({
+    error: esp32_error,
+  });
+  res.status(200).json({ status: "success" });
+});
+
+app.listen(3000);
